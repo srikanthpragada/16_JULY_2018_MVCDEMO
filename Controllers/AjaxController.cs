fd280305266e91b1eabcf2492e8bdc74d1a6f5ee
srikanthpragada <srikanthpragada@gmail.com>
2018-08-14 09:32:01 +0530

    14-AUGF

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Now()
+        {
+            return DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+        }
+
+        public string Prodname(int id)
+        {
+            // Get name for id
+            if (id < 10)
+                return "Product " + id;
+            else
+                return null; 
+        }
+    }
+}
